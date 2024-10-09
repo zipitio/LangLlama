74bbf4691297629742f1249c72de5530f0f8c66e
zipitio <62092328+zipitio@users.noreply.github.com>
2024-10-08 22:49:31 -0400

    added story page and connected form params to story

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -7,9 +7,14 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "name": "Toby"})
+    return templates.TemplateResponse("index.html", {"request": request})
 
+@app.post("/story")
+async def story(request: Request, language: str = Form(...), language_level: str = Form(...), article_topic: str = Form(...)):
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    return templates.TemplateResponse("story.html",{
+        "request": request,
+        "language": language,
+        "language_level": language_level,
+        "article_topic": article_topic
+    })
