d6401bbd0e8241ce6d0da02664cad11916048bbd
zipitio <62092328+zipitio@users.noreply.github.com>
2024-10-25 22:37:51 -0400

    add delete

--- main.py
+++ main.py
@@ -12,14 +12,13 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 templates = Jinja2Templates(directory="templates")
 
+#pydantic used here for data validation
 class TopicBase(BaseModel):
-    topic_id: int
     topic_name: str
     genre: str
 
 class UserBase(BaseModel):
     username: str
-    #user_id: int
 
 # Dependency to create and close sessions for each request
 def get_db():
@@ -31,12 +30,27 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-@app.post("/topics/)", status_code=status.HTTP_201_CREATED)
+@app.post("/topics/", status_code=status.HTTP_201_CREATED)
 async def create_topic(topic: TopicBase, db: db_dependency):
     db_topic = models.Topic(**topic.dict())
     db.add(db_topic)
     db.commit()
     return db_topic.topic_name
+
+@app.get("/topics/{topic_id}", status_code=status.HTTP_200_OK)
+async def read_topic(topic_id: int, db: db_dependency):
+    topic = db.query(models.Topic).filter(models.Topic.id == topic_id).first()
+    if topic is None:
+        raise HTTPException(status_code=404, detail="Topic not found")
+    return topic
+
+@app.delete("/topics/{topic_id}", status_code=status.HTTP_200_OK)
+async def delete_topic(topic_id: int, db: db_dependency):
+    db_topic = db.query(models.Topic).filter(models.Topic.id == topic_id).first()
+    if db_topic is None:
+        raise HTTPException(status_code=404, detail="Topic not found")
+    db.delete(db_topic)
+    db.commit()
 
 @app.post("/users/", status_code=status.HTTP_201_CREATED)
 async def create_user(user: UserBase, db: db_dependency):
@@ -51,6 +65,15 @@ async def read_user(user_id: int, db: db_dependency):
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return user
+
+@app.delete("/users/", status_code=status.HTTP_200_OK)
+async def delete_user(user_id: int, db: db_dependency):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="Topic not found")
+    db.delete(db_user)
+    db.commit()
+
 @app.get("/")
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
