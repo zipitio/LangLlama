00c9b62faa407f5bc2de32a9e875d6a66cd13ac9
zipitio <62092328+zipitio@users.noreply.github.com>
2024-10-25 22:17:17 -0400

    user get and topics post

--- main.py
+++ main.py
@@ -31,12 +31,26 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+@app.post("/topics/)", status_code=status.HTTP_201_CREATED)
+async def create_topic(topic: TopicBase, db: db_dependency):
+    db_topic = models.Topic(**topic.dict())
+    db.add(db_topic)
+    db.commit()
+    return db_topic.topic_name
+
 @app.post("/users/", status_code=status.HTTP_201_CREATED)
 async def create_user(user: UserBase, db: db_dependency):
     db_user = models.User(**user.dict())
     db.add(db_user)
     db.commit()
+    return db_user.username
 
+@app.get("/users/{user_id}", status_code=status.HTTP_200_OK)
+async def read_user(user_id: int, db: db_dependency):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
 @app.get("/")
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
